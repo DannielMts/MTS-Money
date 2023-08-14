bf7314732e78d2ce0b9d14e03ac004806a7ace6d
Danniel <dannieleber@gmail.com>
2023-08-14 09:51:09 -0400

    FIX: POST new Transaction

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
+import { api } from "../../services/api";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -14,8 +15,24 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("");
 
-  const [type, setType] = useState('')
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      category,
+      value,
+      type,
+    };
+
+    api.post("/transactions", data);
+  }
 
   return (
     <ReactModal
@@ -31,30 +48,51 @@ export function NewTransactionModal({
       >
         <img src={closeImg} alt="close modal" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>New Transaction</h2>
-
-        <input type="text" placeholder="Title" />
-        <input type="number" placeholder="Pricing" />
+        <input
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="Price"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
         <TransactionTypeContainer>
           <RadioBox
-            type='button'
-            onClick={() => {setType('deposit')}}
-            isActive={type === 'deposit'}
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
           >
-                <img src={incomeImg} alt="income" />
-                <span>Income</span>
+            <img src={incomeImg} alt="income" />
+            <span>Income</span>
           </RadioBox>
           <RadioBox
-            type='button'
-            onClick={() => {setType('withdraw')}}
-            isActive={type === 'withdraw'}
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
           >
-              <img src={outcomeImg} alt="outcome" />
-              <span>Outcome</span>
+            <img src={outcomeImg} alt="outcome" />
+            <span>Outcome</span>
           </RadioBox>
         </TransactionTypeContainer>
-        <input type="text" placeholder="Category" />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
+
         <button type="submit">Register</button>
       </Container>
     </ReactModal>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,3 +1,4 @@
+import { transparentize } from "polished";
 import styled from "styled-components";
 
 export const Container = styled.form`
@@ -58,13 +59,22 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps{
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+  green: '#33CC95',
+  red: '#e52e4d'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: ${(props)=> props.isActive ? '#aba' : 'transparent'};
+    
+    background: ${(props)=> props.isActive
+    ? transparentize(0.85, colors[props.activeColor])  
+    : 'transparent'};
 
     display: flex;
     align-items: center;
@@ -85,4 +95,5 @@ export const RadioBox = styled.button<RadioBoxProps>`
       font-size: 1rem;
       color: var(--text-title);
     }
+
 `;
